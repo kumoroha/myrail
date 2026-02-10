7c7cc8a1f93d084c86281a8cbb1a5a0fd7ce8b66
kumoroha <199690710+kumoroha@users.noreply.github.com>
2026-02-10 13:51:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function table(datalist) {
+  let input;
+  if (datalist.clear == 'true') {
+    input = '<input type="checkbox" checked>';
+  } else {
+    input= '<input type="checkbox">';
+  }
   const rows = datalist.map(char => 
-    `<tr><td>${char.name}</td><td>${char.title}</td><td>${char.color}</td></tr>`).join('');
+    `<tr><td>${char.name}</td>
+    <td>${char.comp}</td>
+    <td>${char.line}</td>
+    <td>${char.sect}</td>
+    <td>${char.opekm}</td>
+    <td>${char.likm}</td>
+    <td>${char.date}</td>
+    <td>${input}</td>
+    </tr>`).join('');
   document.getElementById('shinkansen').innerHTML = rows;
 }
 table(datalist);
