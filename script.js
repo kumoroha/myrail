88859d9c6a87a3172fe93f56550dd92636263eae
kumoroha <199690710+kumoroha@users.noreply.github.com>
2026-02-10 13:34:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 function table(datalist) {
+  const rows = datalist.map(char => 
+    `<tr><td>${char.name}</td><td>${char.title}</td><td>${char.color}</td></tr>`).join('');
+  document.getElementById('shinkansen').innerHTML = rows;
+}
+table(datalist);
